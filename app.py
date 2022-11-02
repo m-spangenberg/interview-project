7c6fc1e1dfac699f0f48242cd57726a6065d4199
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-02 19:42:06 +0200

    chore: dependencies
    add database and login packages

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from markupsafe import escape
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from models import db, DB_NAME, DB_LOCATION, create_database
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    flash,
+    make_response,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 
+# FLASK CONFIGURATION
 app = Flask(__name__)
+app.secret_key = b"thisIsNotAProductionApp"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_LOCATION}/{DB_NAME}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/")
-def home():
-    return "foo"
+# DATABASE INITIALIZATION
+db.init_app(app)
+create_database(app)
+
+# APP ROUTES
--- models.py
+++ models.py
@@ -1,1 +1,13 @@
-#
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+
+DB_NAME = "forms.db"
+DB_LOCATION = "data/db/"
+
+db = SQLAlchemy()
+
+
+def create_database(app):
+    """Perform a check to see if the database exists"""
+    if not path.exists(DB_LOCATION + DB_NAME):
+        db.create_all(app=app)
