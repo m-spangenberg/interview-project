de4ee33d37ecd9129f7fac878761ae1218ead564
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-04 01:20:27 +0200

    chore: consolidate tables

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -11,6 +11,9 @@ class Applicant(db.Model):
     """applicant table"""
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    state = db.Column(db.Boolean, default=False)
+    duration = db.Column(db.Integer, default="0")
 
 class User(db.Model, UserMixin):
     """superuser table for user authentication"""
@@ -29,11 +32,3 @@ class Questions(db.Model):
     heading = db.Column(db.String(64), default='Question #')
     prompt = db.Column(db.String(256))
     #field = db.Column(ChoiceType(FIELDS))
-
-class Session(db.Model):
-    """per applicant session statistics"""
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    state = db.Column(db.Boolean, default=False)
-    duration = db.Column(db.Integer, default="0")
