bd9b25fc919f1d374ccb2d5d3722823a2932866a
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-04 11:36:44 +0200

    chore: database design tests

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -16,10 +16,10 @@ class Applicant(db.Model):
 
 class ApplicantStats(db.Model):
     """
-    session statistics for applicant
+    session statistics for applicants
     """
     id = db.Column(db.Integer, primary_key=True)
-    # applicant_id = 
+    applicant_id = db.Column(db.Integer, db.ForeignKey('applicant.id'))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     state = db.Column(db.Boolean, default=False)
     duration = db.Column(db.Integer, default="0")
@@ -34,7 +34,7 @@ class User(db.Model, UserMixin):
 
 class FormData(db.Model):
     """
-    answers to applicant's questionnaire session
+    answers to questionnaire sessions
     """
     id = db.Column(db.Integer, primary_key=True)
 
@@ -44,26 +44,40 @@ class FormSession(db.Model):
     version of questionnaire accessed by applicant
     """
     id = db.Column(db.Integer, primary_key=True)
+    applicant_id = db.Column(db.Integer, db.ForeignKey('applicant.id'))
+    history_id = db.Column(db.Integer, db.ForeignKey('applicant.id'))
 
+class FormHistory(db.Model):
+    """
+    history of all questionnaires
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer, db.ForeignKey('applicant.id'))
+    #state = db.relationship('FormState')
 
 class FormState(db.Model):
     """
     structure of the questionnaire
     """
     id = db.Column(db.Integer, primary_key=True)
-    heading = db.Column(db.String(64), default='Question #')
-    prompt = db.Column(db.String(256))
-    #field = db.Column(ChoiceType(FIELDS))
+    #history_id = db.Column(db.Integer, db.ForeignKey('formhistory.id'))
+    #question = db.relationship('FormQuestion')
 
 class FormQuestion(db.Model):
     """
     questions available to form
     """
     id = db.Column(db.Integer, primary_key=True)
-
+    #state_id = db.Column(db.Integer, db.ForeignKey('formstate.id'))
+    question = db.Column(db.String(256))
+    #choice = db.relationship('FormChoice')
 
 class FormChoice(db.Model):
     """
     choices available to form questions
     """
     id = db.Column(db.Integer, primary_key=True)
+    #question_id = db.Column(db.Integer, db.ForeignKey('formquestion.id'))
+
+    # TODO: perform choice validation to protect integrity
+    choice_type = db.Column(db.String(16))
