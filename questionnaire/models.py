6e9ffd5f8a66e6cf39c46ea2fe9611c8175ba9e6
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-04 10:59:59 +0200

    chore: model tests

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -8,27 +8,62 @@ db = SQLAlchemy()
 
 # Database Models
 class Applicant(db.Model):
-    """applicant table"""
+    """
+    applicants table
+    """
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True)
+
+class ApplicantStats(db.Model):
+    """
+    session statistics for applicant
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    # applicant_id = 
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     state = db.Column(db.Boolean, default=False)
     duration = db.Column(db.Integer, default="0")
 
 class User(db.Model, UserMixin):
-    """superuser table for user authentication"""
+    """
+    users table
+    """
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(128))
 
 class FormData(db.Model):
-    """from representation table used for view generation"""
+    """
+    answers to applicant's questionnaire session
+    """
     id = db.Column(db.Integer, primary_key=True)
-    #user response
 
-class Questions(db.Model):
-    """question structure table used by form builder"""
+
+class FormSession(db.Model):
+    """
+    version of questionnaire accessed by applicant
+    """
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class FormState(db.Model):
+    """
+    structure of the questionnaire
+    """
     id = db.Column(db.Integer, primary_key=True)
     heading = db.Column(db.String(64), default='Question #')
     prompt = db.Column(db.String(256))
     #field = db.Column(ChoiceType(FIELDS))
+
+class FormQuestion(db.Model):
+    """
+    questions available to form
+    """
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class FormChoice(db.Model):
+    """
+    choices available to form questions
+    """
+    id = db.Column(db.Integer, primary_key=True)
