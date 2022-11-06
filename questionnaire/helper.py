14ba0bfa936db336bb4b2390549b3407a02b3f06
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-06 17:42:52 +0200

    chore: helper functions

--- questionnaire/helper.py
+++ questionnaire/helper.py
@@ -1,11 +1,31 @@
 from flask import current_app
 
 from questionnaire.models import db
+from questionnaire.models import FormSession
 from questionnaire.models import Applicant
 from questionnaire.models import FormState
 from questionnaire.models import User
 
 from werkzeug.security import generate_password_hash
+
+from random import choice
+from random import randrange
+
+
+def del_applicant(applicantid: str) -> None:
+    '''delete the applicant and all their associated data from the database'''
+    try:
+        if applicantid:
+            with current_app.app_context():
+
+                applicant = Applicant.query.filter_by(email=applicantid).first()
+                db.session.delete(applicant)
+                db.session.commit()
+
+        raise Exception('no valid applicant id supplied')
+
+    except Exception as e:
+        current_app.logger.error('%s -- failed to delete applicant data', e)
 
 
 def check_email(email: str) -> str:
@@ -26,10 +46,6 @@ def gen_superuser(email: str, password: str) -> None:
     check if db is empty
     check if user does not exists
     generate superuser in database
-
-    NOTE:
-    because of how Flask handles the application context this will fire
-    off an exception if automatic template reloading is on in flasks config.
     '''
     try:
         with current_app.app_context():
@@ -51,10 +67,6 @@ def gen_superuser(email: str, password: str) -> None:
 def gen_default_form() -> None:
     '''
     generate default questions form
-    
-    NOTE:
-    because of how Flask handles the application context this will fire
-    off an exception if automatic template reloading is on in flasks config.
     '''
     try:
         with current_app.app_context():
@@ -93,3 +105,70 @@ def gen_default_form() -> None:
 
     except Exception as e:
         current_app.logger.error('%s -- failed to populate default form', e)
+
+
+def gen_applicants(count: int) -> None:
+    '''
+    generate dummy applicants
+    '''
+
+    domains = ['bigmail', 'yeehaw', 'icebox', 'ycloud', 'neutron', 'inlook', 'lmao', 'windex']
+    tlds = ['com', 'org', 'io', 'net', 'mail', 'spam']
+    f_names = ['test', 'spam', 'leetcoder', 'dbdude', 'htmlmao', 'howto']
+    l_names = ['magic', 'box', 'hunter', 'cool', 'super', 'bingbong']
+
+    try:
+        with current_app.app_context():
+
+            if Applicant.query.first() is None:
+
+                for _ in range(count):
+                    email = f'{choice(f_names)}{choice(l_names)}@{choice(domains)}.{choice(tlds)}'
+                    state = 1
+                    duration = randrange(5, 120, 1)
+
+                    db.session.add(
+                        Applicant(
+                            email=email,
+                            state=state,
+                            duration=duration,
+                        )
+                    )
+
+                db.session.commit()
+
+            else:
+                raise Exception('applicant pool is already pre-populated')
+
+    except Exception as e:
+        current_app.logger.error('%s -- failed to populate default applicant pool', e)
+
+
+def gen_sessions() -> None:
+    '''
+    generate dummy sessions for available applicants
+    '''
+    try:
+        with current_app.app_context():
+
+            if FormSession.query.first() is None:
+
+                applicants = Applicant.query.all()
+
+                for applicant in applicants:
+
+                    db.session.add(
+                        FormSession(
+                            applicant_id=applicant.id,
+                            answer="This is just some dummy text so there's something to look at.",
+                            question="This is just here so the form is populated"
+                        )
+                    )
+
+                db.session.commit()
+
+            else:
+                raise Exception('session is already pre-populated')
+
+    except Exception as e:
+        current_app.logger.error('%s -- failed to populate default session data', e)
