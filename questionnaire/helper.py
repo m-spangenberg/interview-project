b0607cc7d155c06b221696ca27bde709fa24633f
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-06 14:47:17 +0200

    chore: logging

--- questionnaire/helper.py
+++ questionnaire/helper.py
@@ -1,3 +1,5 @@
+from flask import current_app
+
 from questionnaire.models import db
 from questionnaire.models import Applicant
 from questionnaire.models import FormState
@@ -13,53 +15,81 @@ def check_email(email: str) -> str:
         if email_check.email is not None:
             email_exists = str(email_check.email)
     except AttributeError:
+        current_app.logger.info('login attempt by processed applicant: %s', email)
         email_exists = None
 
     return email_exists
 
 
-def gen_superuser(email: str, password: str) -> str:
+def gen_superuser(email: str, password: str) -> None:
     '''
     check if db is empty
     check if user does not exists
     generate superuser in database
+
+    NOTE:
+    because of how Flask handles the application context this will fire
+    off an exception if automatic template reloading is on in flasks config.
     '''
-    if User.query.first() is not None:
+    try:
+        with current_app.app_context():
+            if User.query.first() is None:
+                if not User.query.filter_by(email=email).first():
 
-        if not User.query.filter_by(email=email).first():
+                    password = generate_password_hash(password)
+                    superuser = User(email=email, password=password)
+                    db.session.add(superuser)
+                    db.session.commit()
+                else:
+                    raise Exception('superuser already exists')
+            else:
+                raise Exception('database is not empty')
 
-            # Create Superuser
-            password = generate_password_hash(password)
-            superuser = User(email=email, password=password)
-            db.session.add(superuser)
+    except Exception as e:
+        current_app.logger.error('%s -- failed to generate superuser', e)
 
-
-def gen_defaults():
+def gen_default_form() -> None:
     '''
-    generate form with default questions
+    generate default questions form
+    
+    NOTE:
+    because of how Flask handles the application context this will fire
+    off an exception if automatic template reloading is on in flasks config.
     '''
-    db.session.add(
-        FormState(
-            version=1,
-            question="Where did you hear from us, and what do you think will make\
-                    you a great asset to the BCX Business Application Department?",
-            input_type="input",
-            choice=None,
-        )
-    )
-    db.session.add(
-        FormState(
-            version=1,
-            question="How many software solutions did you write in your life?",
-            input_type="select",
-            choice="1 to 5;6 -25;26 -100;101 +",
-        )
-    )
-    db.session.add(
-        FormState(
-            version=1,
-            question="Was it fun building a website for an interview?",
-            input_type="radio",
-            choice="yes;no",
-        )
-    )
+    try:
+        with current_app.app_context():
+
+            if FormState.query.first() is None:
+
+                db.session.add(
+                    FormState(
+                        version=1,
+                        question="Where did you hear from us, and what do you think will make\
+                                you a great asset to the BCX Business Application Department?",
+                        input_type="input",
+                        choice=None,
+                    )
+                )
+                db.session.add(
+                    FormState(
+                        version=1,
+                        question="How many software solutions did you write in your life?",
+                        input_type="select",
+                        choice="1 to 5;6 -25;26 -100;101 +",
+                    )
+                )
+                db.session.add(
+                    FormState(
+                        version=1,
+                        question="Was it fun building a website for an interview?",
+                        input_type="radio",
+                        choice="yes;no",
+                    )
+                )
+
+                db.session.commit()
+            else:
+                raise Exception('form is already pre-populated')
+
+    except Exception as e:
+        current_app.logger.error('%s -- failed to populate default form', e)
