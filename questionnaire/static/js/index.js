0d84c361972a7748287ef5e629319b70684b7e30
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-10 21:59:54 +0200

    chore: form builder logic

--- questionnaire/static/js/index.js
+++ questionnaire/static/js/index.js
@@ -23,21 +23,29 @@ $(document).on('click', '#addEntry', function() {
     let input_options = $("input[name=entryOptionField]").val();
     let input_types = $("select[name=entryInputTypeField] option:checked").val();
 
+    // Set empty options to None for consistency
+    if (!input_options){
+        input_options = 'None';
+    }
+
+    // Capitalize input type
+    let input_capitalize = input_types[0].toUpperCase() + input_types.slice(1).toLowerCase();
+        
+    // construct entry
     $entryDiv = $(
         `<div id="x" class="box is-shadowless my-2 mx-2">
         <button id="deleteEntry" class="delete is-medium is-pulled-right"></button>
         <label id="x" class="label">Q: ${question} </label>
-        <label id="x" class="label">A: ${input_types}, (${input_options})</label>
+        <label id="x" class="label">A: ${input_capitalize}, (${input_options})</label>
         </div>`
     );
 
     $($entryDiv).insertBefore("#entryCreation");
 });
 
-// form-builder -- only enable input options for select and radio in dropdown
 
+// form-builder -- only enable input options for select and radio in dropdown
 $(document).ready(function() {
-    // ensure option field is disabled on load (input is default)
     $("input[name=entryOptionField]").prop('disabled', true);
 });
 
@@ -47,7 +55,10 @@ $(document).on('click', 'select[name=entryInputTypeField]', function() {
     let selectedInputType = $("select[name=entryInputTypeField] option:checked").val();
 
     if (selectedInputType === 'input') {
+        // disable field
         $("input[name=entryOptionField]").prop('disabled', true);
+        // clear field
+        $("input[name=entryOptionField]").val('');
     } else {
         $("input[name=entryOptionField]").prop('disabled', false);
     }
