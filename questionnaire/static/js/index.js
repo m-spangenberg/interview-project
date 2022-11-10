c9644d9376ac26065597b2b3ffd7992db5dfd80c
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-10 17:12:43 +0200

    chore: entry submission logic

--- questionnaire/static/js/index.js
+++ questionnaire/static/js/index.js
@@ -9,13 +9,47 @@ burgerIcon.addEventListener('click', () => {
 
 // form-builder -- delete entry from page
 
-$(document).on('mouseenter', '#deleteEntry', function () {
-    $(this).find(":button").show();
-}).on('mouseleave', '.divbutton', function () {
-    $(this).find(":button").hide();
-}).on('click', ':button', function() {
+$(document).on('click', '#deleteEntry', function () {
+    $(this).find("#deleteEntry");
+    }).on('click', '#deleteEntry', function() {
     $(this).parent().remove();
 });
 
-// form-builder -- add question to builder page
+// form-builder -- add entry to builder page
+
+$(document).on('click', '#addEntry', function() {
+
+    let question = $("input[name=entryTextField]").val();
+    let input_options = $("input[name=entryOptionField]").val();
+    let input_types = $("select[name=entryInputTypeField] option:checked").val();
+
+    $entryDiv = $(
+        `<div id="x" class="box is-shadowless my-2 mx-2">
+        <button id="deleteEntry" class="delete is-medium is-pulled-right"></button>
+        <label id="x" class="label">Q: ${question} </label>
+        <label id="x" class="label">A: ${input_types}, (${input_options})</label>
+        </div>`
+    );
+
+    $($entryDiv).insertBefore("#entryCreation");
+});
+
+// form-builder -- only enable input options for select and radio in dropdown
+
+$(document).ready(function() {
+    // ensure option field is disabled on load (input is default)
+    $("input[name=entryOptionField]").prop('disabled', true);
+});
+
+
+$(document).on('click', 'select[name=entryInputTypeField]', function() {
+
+    let selectedInputType = $("select[name=entryInputTypeField] option:checked").val();
+
+    if (selectedInputType === 'input') {
+        $("input[name=entryOptionField]").prop('disabled', true);
+    } else {
+        $("input[name=entryOptionField]").prop('disabled', false);
+    }
+});
 
