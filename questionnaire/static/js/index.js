906beaee44ce68053d7474e036fed480aa39fcc8
m-spangenberg <mspangenberg.dev@gmail.com>
2022-11-10 12:03:33 +0200

    chore: add entry deletion

--- questionnaire/static/js/index.js
+++ questionnaire/static/js/index.js
@@ -6,3 +6,16 @@ const navbarMenu = document.querySelector('#navbarLinks');
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
+
+// form-builder -- delete entry from page
+
+$(document).on('mouseenter', '#deleteEntry', function () {
+    $(this).find(":button").show();
+}).on('mouseleave', '.divbutton', function () {
+    $(this).find(":button").hide();
+}).on('click', ':button', function() {
+    $(this).parent().remove();
+});
+
+// form-builder -- add question to builder page
+
